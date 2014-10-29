e786bbec62ddd999641e20a9dba66c0b80ed4929
Tom Janson <priv.tom.janson@gmail.com>
2014-10-29 21:30:10 +0100

    cleaner pin conversion & validitation

--- pi-gpio.js
+++ pi-gpio.js
@@ -2,7 +2,7 @@
 
 var gpioUtil = require("pi-gpioutil");
 
-var physToBcm = require("./pinMap.js").physToBcm();
+var physToBcm = require("./pinMap.js").physToBcm;
 var physToWiring = require("./pinMap.js").physToWiring;
 
 var outputPins = [];
@@ -15,14 +15,14 @@ function noop(){};
 var gpio = {
     read: function(physPin, callback, exportMode) {
         function readVal() {
-            gpioUtil.read(physToWiring[physPin], function(err, stdout, stderr, boolVal) {
+            gpioUtil.read(physToWiring(physPin), function(err, stdout, stderr, boolVal) {
                 var intVal = boolVal ? 1 : 0;
                 (callback || noop)(err, intVal);
             });
         }
 
         if ((inputPins.indexOf(physPin) === -1 && exportMode !== 'off') || exportMode === 'force') {
-            gpioUtil.export(physToBcm[physPin], "in", function(err, stdout, stderr) {
+            gpioUtil.export(physToBcm(physPin), "in", function(err, stdout, stderr) {
                 if (!err) {
                     outputPins = outputPins.filter(function(e) { return e !== physPin; });
                     inputPins.push(physPin);
@@ -38,15 +38,15 @@ var gpio = {
 
     write: function(physPin, value, callback, exportMode) {
         if ((outputPins.indexOf(physPin) === -1 && exportMode !== 'off') || exportMode === 'force') {
-            gpioUtil.export(physToBcm[physPin], "out", function(err, stdout, stderr) {
+            gpioUtil.export(physToBcm(physPin), "out", function(err, stdout, stderr) {
                 if (!err) {
                     inputPins = inputPins.filter(function(e) { return e !== physPin; });
                     outputPins.push(physPin);
-                    gpioUtil.write(physToWiring[physPin], value, (callback || noop));
+                    gpioUtil.write(physToWiring(physPin), value, (callback || noop));
                 }
             });
         } else {
-            gpioUtil.write(physToWiring[physPin], value, function(err, stdout, stderr) {
+            gpioUtil.write(physToWiring(physPin), value, function(err, stdout, stderr) {
                 (callback || noop)(err);
             });
         }
@@ -61,7 +61,7 @@ var gpio = {
 
         var options = parseOptions(optionsString);
 
-        gpioUtil.export(physToBcm[physPin], options.direction, function(err, stdout, stderr) {
+        gpioUtil.export(physToBcm(physPin), options.direction, function(err, stdout, stderr) {
             if (err) {
                 console.error("ERROR [pi-gpio] failed to export pin " + physPin);
             }
@@ -72,7 +72,7 @@ var gpio = {
             }
 
             if (typeof options.pull !== 'undefined') {
-                gpioUtil.mode(physToWiring[physPin], options.pull, (callback || noop));
+                gpioUtil.mode(physToWiring(physPin), options.pull, (callback || noop));
             } else {
                 (callback || noop)(err);
             }
@@ -80,7 +80,7 @@ var gpio = {
     },
 
     unexport: function(physPin, callback) {
-        gpioUtil.unexport(physToBcm[physPin], function(err, stdout, stderr) {
+        gpioUtil.unexport(physToBcm(physPin), function(err, stdout, stderr) {
             if (err) {
                 console.error("ERROR [pi-gpio] failed to unexport pin " + physPin);
             }
--- pinMap.js
+++ pinMap.js
@@ -1,80 +1,62 @@
 var fs = require('fs');
 
-var rev = fs.readFileSync("/proc/cpuinfo").toString().split("\n").filter(function(line) {
+// http://elinux.org/RPi_HardwareHistory#Board_Revision_History
+var rev = parseInt(fs.readFileSync("/proc/cpuinfo").toString().split("\n").filter(function(line) {
               return line.indexOf("Revision") == 0;
-          })[0].split(":")[1].trim();
+          })[0].split(":")[1].trim(), 16) < 3 ? 1 : 2;
 
-rev = parseInt(rev, 16) < 3 ? 1 : 2; // http://elinux.org/RPi_HardwareHistory#Board_Revision_History
+// physical pin to [bcm, wiring]
+const bcmIndex = 0;
+const wpiIndex = 1;
+const pinMap = {
+    "3": (rev === 2 ? [2, 8] : [0, 8]),
+    "5": (rev === 2 ? [5, 9] : [1, 9]),
+    "7": [4, 7],
+    "8": [14, 15],
+    "10": [15, 16],
+    "11": [17, 0],
+    "12": [18, 1],
+    "13": (rev === 2 ? [27, 2]: [21, 2]),
+    "15": [22, 3],
+    "16": [23, 4],
+    "18": [24, 5],
+    "19": [10, 12],
+    "21": [9,  13],
+    "22": [25, 6],
+    "23": [11, 14],
+    "24": [8,  10],
+    "26": [7,  11],
 
-const baseMap = {
-    "3": 0,
-    "5": 1,
-    "7": 4,
-    "8": 14,
-    "10": 15,
-    "11": 17,
-    "12": 18,
-    "13": 21,
-    "15": 22,
-    "16": 23,
-    "18": 24,
-    "19": 10,
-    "21": 9,
-    "22": 25,
-    "23": 11,
-    "24": 8,
-    "26": 7,
-
-    // Model B+ pins
-    "29": 5,
-    "31": 6,
-    "32": 12,
-    "33": 13,
-    "35": 19,
-    "36": 16,
-    "37": 26,
-    "38": 20,
-    "40": 21
+    // Model  B+ pins
+    "29": [5,  21],
+    "31": [6,  22],
+    "32": [12, 26],
+    "33": [13, 23],
+    "35": [19, 24],
+    "36": [16, 27],
+    "37": [26, 25],
+    "38": [20, 28],
+    "40": [21, 29]
 };
 
-var physToBcm = function() {
-    var map = JSON.parse(JSON.stringify(baseMap));
-    if (rev == 2) {
-        map["3"] = 2;
-        map["5"] = 3;
-        map["13"] = 27;
+function convert(pinNumber, mapIndex) {
+    pinNumber = parseInt(pinNumber, 10);
+    if (typeof pinNumber !== 'number') {
+        throw new Error("Pin number isn't valid [1]");
     }
-    return map;
+    if (typeof pinMap[pinNumber] !== 'object') {
+        throw new Error("Pin number isn't valid [2]");
+    }
+    if (typeof pinMap[pinNumber][mapIndex] !== 'number') {
+        throw new Error("Pin number isn't valid [3]");
+    }
+    return pinMap[pinNumber][mapIndex];
 }
 
-const physToWiring = {
-    "3": 8,
-    "5": 9,
-    "7": 7,
-    "8": 15,
-    "10": 16,
-    "11": 0,
-    "12": 1,
-    "13": 2,
-    "15": 3,
-    "16": 4,
-    "18": 5,
-    "19": 12,
-    "21": 13,
-    "22": 6,
-    "23": 14,
-    "24": 10,
-    "26": 11,
-    "29": 21,
-    "31": 22,
-    "32": 26,
-    "33": 23,
-    "35": 24,
-    "36": 27,
-    "37": 25,
-    "38": 28,
-    "40": 29
+module.exports.physToBcm = function(physPin) {
+    return convert(physPin, bcmIndex);
 }
 
-module.exports.physToBcm = physToBcm;
-module.exports.physToWiring = physToWiring;
+module.exports.physToWiring = function(physPin) {
+    return convert(physPin, wpiIndex);
+}
